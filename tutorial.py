7846ce0f8743319ccf208a56ab4ae77225904e35
christorrella <christorrella@gmail.com>
2021-10-10 17:09:45 -0400

    complete tutorial 3, page navigating and clicking elements (https://youtu.be/U6gbGk5WPws\?list\=PLzMcBGfZo4-n40rB1XaJ0ak1bemvlqumQ)

--- tutorial.py
+++ tutorial.py
@@ -5,10 +5,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Excplicit Waits, see 5.1 from https://selenium-python.readthedocs.io/waits.html
+# Explicit waits
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 # Specify path for the Chrome webdriver
 PATH = "./driver/chromedriver"
@@ -19,30 +20,28 @@ driver = webdriver.Chrome(PATH)
 # Open the Chrome browser with the http target of Google
 driver.get("https://www.techwithtim.net/")
 
-print(driver.title)
 
-# find first element with this HTML name
-search = driver.find_element_by_name("s")
-# send keys to that element
-search.send_keys("test")
-# hit return to search the website
-search.send_keys(Keys.RETURN)
-
-# Explicitly wait up to 10 seconds for the "main" id to show up in the DOM
+# Click three links on pages by link text and ID, and then go back in browser to root website
+# Using explicit waits
 try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
+    link = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Python Programming"))
     )
-    # once we have "main", find all of the articles by tag name
-    articles = main.find_elements_by_tag_name("article")
-    # for each of those articles, print the text within
-    for article in articles:
-        header = article.find_element_by_class_name("entry-summary")
-        print(header.text)
+    link.click()
 
+    link = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
+    )
+    link.click()
 
-    # wait and then close
-    time.sleep(5)
-    driver.quit()
-finally:
+    link = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "sow-button-19310003"))
+    )
+    link.click()
+
+    driver.back()
+    driver.back()
+    driver.back()
+
+except:
     driver.quit()
